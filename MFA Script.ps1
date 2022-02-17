3d953b34d52a9551dd33046febed534b11e06200
austinfhts <99911828+austinfhts@users.noreply.github.com>
2022-02-17 11:38:41 -0800

    Add files via upload

---
+++ MFA Script.ps1
@@ -0,0 +1,9 @@
+﻿Connect-MgGraph -Scopes UserAuthenticationMethod.ReadWrite.All
+Select-MgProfile -Name beta
+
+Import-CSV "C:\Users\AustinF.LBANET\Documents\mfasetup.csv" |ForEach-Object {
+$email = $_.Email
+$mobile = $_.Mobile
+$formatted = "+1 " + $mobile
+New-MgUserAuthenticationPhoneMethod -UserId $email -PhoneType "mobile" -PhoneNumber $formatted
+}
---
+++ exchange calendars.ps1
@@ -0,0 +1,22 @@
+﻿$emails = Get-Mailbox | Select-Object Alias
+$users = Get-Mailbox | Select-Object Alias
+
+$emails | foreach {
+    if($_.Alias -ne "Larry" -or $_.Alias -ne "ShainaC" -or $_.Alias -ne "SteveD")
+    {
+        $identity = $_.Alias
+        $calendar = $identity + ":\calendar"
+        Write-Host $calendar
+        $users | foreach {
+        if ($_.Alias -ne $identity) {
+        Write-Host $_.Alias
+        Add-MailboxFolderPermission -Identity $calendar -User $_.Alias -AccessRights Reviewer
+        }
+        }
+    }
+    else
+    {
+    continue
+    }
+}
+
